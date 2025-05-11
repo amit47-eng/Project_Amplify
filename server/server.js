c3e0d50f295a97366d67be796c62c725fb68fe93
amit47-eng <av495267@gmail.com>
2025-05-11 22:09:38 +0530

    change

--- server/server.js
+++ server/server.js
@@ -89,7 +89,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+module.exports = app
